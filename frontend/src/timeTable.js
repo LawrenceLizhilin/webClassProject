10eea7bc9cacde933425ed5a735fc845ed49a451
LawrenceLizhilin <lawrencelzl1922@gmail.com>
2021-12-12 19:49:03 +0800

    style:fix column

--- frontend/src/timeTable.js
+++ frontend/src/timeTable.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import {useState, useMemo, useEffect} from 'react';
-import {Tabs, DatePicker, Space, Table} from 'antd';
+import { useState, useMemo, useEffect } from 'react';
+import { Tabs, DatePicker, Space, Table } from 'antd';
 import moment from 'moment';
 
-const {TabPane} = Tabs;
+const { TabPane } = Tabs;
 const columns = [
     {
         title: '小組',
-        dataIndex: 'group',
-        key: 'group',
+        dataIndex: 'dept',
+        fixed: 'left',
+        key: 'dept',
         width: 100
     }, {
         title: '姓名',
         dataIndex: 'name',
+        fixed:'left',
         key: 'name',
         width: 100
     },
 ]
 
 export default function Timetable() {
-    let [date, setDate] = useState (moment().add(-10,'month'));
+    let [date, setDate] = useState(moment().add(-10, 'month'));
     let [pickValue, setPickValue] = useState(moment());
+    
     function mGetDate(tempDate) {
         let year = moment(tempDate).toDate().getFullYear();
         let month = moment(tempDate).toDate().getMonth() + 1;
@@ -28,16 +31,16 @@ export default function Timetable() {
         return d.getDate();
     }
     // useEffect(() => {
-        
+
     // }, [date]);
 
     const memorizedColumns = useMemo(() => {
-        let clonedColumns = [... columns];
+        let clonedColumns = [...columns];
         for (let i = 1; i <= mGetDate(date); i++) {
             let num = i.toString();
-            clonedColumns.push({key: num, title: num, width: 60, dataIndex: num});
+            clonedColumns.push({ key: num, title: num, width: 60, dataIndex: num });
         }
-        clonedColumns.push({key: '加班總時長', title: '加班總時長', width: 160, dataIndex: 'totalHour'});
+        clonedColumns.push({ key: '加班總時長', title: '加班總時長', width: 150, dataIndex: 'totalHour' });
         return clonedColumns;
     }, [date]);
 
@@ -51,20 +54,22 @@ export default function Timetable() {
                             setPickValue(value);
                             setDate(value);
                         }
-                    }/>
+                    } />
             </div>
-        <div>
-            <Tabs defaultActiveKey="1">
-                <TabPane tab="Tab 1" key="1">
-                    <Table columns={memorizedColumns}/>
-                </TabPane>
-                <TabPane tab="Tab 2" key="2">
-                    Content of Tab Pane 2
-                </TabPane>
+            <div>
+                <Tabs defaultActiveKey="1">
+                    <TabPane tab="加班日程表" key="1">
+                        <Table columns={memorizedColumns} scroll={{ x: 'max-content', y: 800 }}
+                            pagination={false}
+                            bordered />
+                    </TabPane>
+                    <TabPane tab="Tab 2" key="2">
+                        Content of Tab Pane 2
+                    </TabPane>
 
-            </Tabs>
+                </Tabs>
+            </div>
         </div>
-    </div>
 
     )
 }
